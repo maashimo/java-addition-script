fa868b45afe8ef4f77078956a8b754aa1716ba3e
Your Name <your.email@example.com>
2025-03-06 14:18:59 +0530

    Initial commit: Added SimpleAddition Java program and README

---
+++ README.md
@@ -0,0 +1,8 @@
+# Simple Addition Program
+
+This is a simple Java program that performs the addition of two numbers and prints the result.
+
+## How to Run
+1. Compile the program:
+   ```bash
+   javac SimpleAddition.java
---
+++ SimpleAddition.java
@@ -0,0 +1,8 @@
+public class SimpleAddition {
+    public static void main(String[] args) {
+        int number1 = 5;
+        int number2 = 10;
+        int sum = number1 + number2;
+        System.out.println("Sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
